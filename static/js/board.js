d5e11f03a7c5607e74e7b8234516ae60255953c7
pdc <pdc@alleged.org.uk>
2012-05-28 21:34:58 +0100

    Rejig JavaScript to hopefully make it work on IE

--- static/js/board.js
+++ static/js/board.js
@@ -3,51 +3,54 @@
 var CardGrid = (function ($) {
     // Poll for updates from the server.
     var pendingPoll;
+    var pendingPollUrl;
 
     function pollForUpdates(url) {
-        // XXX som,ethjign to prevent overlapping polling.
+        if (!$('body').hasClass('polling')) {
+            $('body').addClass('polling');
+            $.ajax(url, {type: 'GET', dataType: 'json'})
+                .done(onPollSuccess)
+                .always(onPollComplete);
+        }
+    }
 
-        $('body').addClass('polling');
-        $.ajax(url, {
-            type: 'GET',
-            dataType: 'json',
-            success: function (data, textStatus, jqXHR) {
-                if (data.ready && data.events) {
-                    for (var i = 0; i < data.events.length; ++i) {
-                        var event = data.events[i];
-                        if (event.type === 'rearrange' && event.dropped && event.order) {
-                            var droppedSelector = '#card-' + event.dropped;
-                            var succID = event.order[1];
-                            if (succID === null) {
-                                // This card was dropped at the end of its bin.
-                                var binSelector = (event.tags && event.tags.length > 0
-                                        ? '#bin-' + event.tags.join('-')
-                                        : '#untagged-bin');
-                                 $(droppedSelector).appendTo(binSelector);
-                                console.log(droppedSelector + '.appendTo ' + binSelector);
-                            } else {
-                                // This card was dropped before another.
-                                var succSelector = '#card-' + succID;
-                                $(droppedSelector).insertBefore(succSelector);
-                                console.log(droppedSelector + '.insertBefore ' + succSelector);
-                            }
-                        }
+    function onPollSuccess(data, textStatus, jqXHR) {
+        if (data.ready && data.events) {
+            for (var i = 0; i < data.events.length; ++i) {
+                var event = data.events[i];
+                if (event.type === 'rearrange' && event.dropped && event.order) {
+                    var droppedSelector = '#card-' + event.dropped;
+                    var succID = event.order[1];
+                    if (succID === null) {
+                        // This card was dropped at the end of its bin.
+                        var binSelector = (event.tags && event.tags.length > 0
+                                ? '#bin-' + event.tags.join('-')
+                                : '#untagged-bin');
+                         $(droppedSelector).appendTo(binSelector);
+                        console.log(droppedSelector + '.appendTo ' + binSelector);
+                    } else {
+                        // This card was dropped before another.
+                        var succSelector = '#card-' + succID;
+                        $(droppedSelector).insertBefore(succSelector);
+                        console.log(droppedSelector + '.insertBefore ' + succSelector);
                     }
                 }
-
-                if (data.next) {
-                    pendingPoll = window.setTimeout(
-                        function () {
-                            pendingPoll = null;
-                            pollForUpdates(data.next);
-                        },
-                        data.pleaseWait || 100);
-                }
-            },
-            complete: function (jqXHT, textStatus) {
-                $('body').removeClass('polling');
             }
-        });
+        }
+
+        if (data.next) {
+            pendingPollUrl = data.next;
+            pendingPoll = window.setTimeout(pollAgain, data.pleaseWait || 100);
+        }
+    }
+
+    function onPollComplete(jqXHT, textStatus) {
+        $('body').removeClass('polling');
+    }
+
+    function pollAgain() {
+        pendingPoll = null;
+        pollForUpdates(pendingPollUrl);
     }
 
     return {
@@ -70,7 +73,14 @@ $(function () {
             for (var i = 0; i < eltIDs.length; ++i) {
                 cardIDs.push(eltIDs[i].replace(/^card-/, ''));
             }
-            var droppedIndex = cardIDs.indexOf(droppedID);
+
+            // Turns out MSIE does not support Array.indexOf.
+            var droppedIndex  = cardIDs.length;
+            while(--droppedIndex >= 0) {
+                if  (cardIDs[droppedIndex] == droppedID) {
+                    break;
+                }
+            }
             if (droppedIndex >= 0) {
                 var succID = (droppedIndex + 1 < cardIDs.length ? cardIDs[droppedIndex + 1] : '-');
                 var abbreviatedIDs = [droppedID, succID];
@@ -89,14 +99,9 @@ $(function () {
                     $.ajax(url, {
                         type: 'POST',
                         dataType: 'json',
-                        data: data,
-                        success: function (data, textStatus, jqXHR) {
-                            //console.log(data);
-                            //console.log(textStatus);
-                        },
-                        complete: function (jqXHR, textStatus) {
-                            ui.item.removeClass('ajax-loading');
-                        }
+                        data: data
+                    }).always(function () {
+                        ui.item.removeClass('ajax-loading');
                     });
                 } else {
                     $('input[name=order]', form$).val(abbreviatedIDs.join(' '));
