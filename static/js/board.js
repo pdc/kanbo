a77cc3165b5d09837f4813ef59c1390434367446
pdc <pdc@alleged.org.uk>
2012-06-08 20:17:23 +0100

    Add close button to messages box & stop it ruining layout

--- static/js/board.js
+++ static/js/board.js
@@ -58,61 +58,74 @@ var CardGrid = (function ($) {
     }
 
     function enableRearrange(ajaxUrl) {
-        // Make the card bins be sortable with drag-and-drop.
-        $('.card-bin').sortable({
-            connectWith: '.card-bin',
-            update: function (event, ui) {
-                // Report the change to the server.
-                var droppedID = ui.item.attr('id').replace(/^card-/, '');
-                var eltIDs = $(this).sortable('toArray');
-                var cardIDs = [];
+      // Make the card bins be sortable with drag-and-drop.
+      $('.card-bin').sortable({
+        connectWith: '.card-bin',
+        update: function (event, ui) {
+            // Report the change to the server.
+            var droppedID = ui.item.attr('id').replace(/^card-/, '');
+            var eltIDs = $(this).sortable('toArray');
+            var cardIDs = [];
+            for (var i = 0; i < eltIDs.length; ++i) {
+                cardIDs.push(eltIDs[i].replace(/^card-/, ''));
+            }
 
-                for (var i = 0; i < eltIDs.length; ++i) {
-                    cardIDs.push(eltIDs[i].replace(/^card-/, ''));
-                }
-
-                // Turns out MSIE does not support Array.indexOf.
-                var droppedIndex  = cardIDs.length;
-                while(--droppedIndex >= 0) {
-                    if  (cardIDs[droppedIndex] == droppedID) {
-                        break;
-                    }
-                }
-                if (droppedIndex >= 0) {
-                    var succID = (droppedIndex + 1 < cardIDs.length ? cardIDs[droppedIndex + 1] : '-');
-                    var abbreviatedIDs = [droppedID, succID];
-
-                    var form$ = $(this).parent().find('form');
-                    if (ajaxUrl) {
-                        var data = {}
-                        var es = $('input[type=hidden]', form$).get();
-                        for (var i = 0; i < es.length; ++i) {
-                            data[es[i].name] = es[i].value;
-                        }
-                        data[ 'order'] =  abbreviatedIDs.join(' ');
-                        data['dropped'] = droppedID;
-                        ui.item.addClass('ajax-loading');
-                        $.ajax(ajaxUrl, {
-                            type: 'POST',
-                            dataType: 'json',
-                            data: data
-                        }).error(function (jqXHT) {
-                            CardGrid.addMessage('Failed to store the new position of the card on the server: refresh this page to see their real positions.')
-                        }).always(function () {
-                            ui.item.removeClass('ajax-loading');
-                        });
-                    } else {
-                        $('input[name=order]', form$).val(abbreviatedIDs.join(' '));
-                        $('input[name=dropped]', form$).val(droppedID);
-                    }
+            // Turns out MSIE does not support Array.indexOf.
+            var droppedIndex  = cardIDs.length;
+            while(--droppedIndex >= 0) {
+                if  (cardIDs[droppedIndex] == droppedID) {
+                    break;
                 }
             }
-        })
-        .disableSelection()
-        .parent().find('form').hide();
+            if (droppedIndex >= 0) {
+                var succID = (droppedIndex + 1 < cardIDs.length ? cardIDs[droppedIndex + 1] : '-');
+                var abbreviatedIDs = [droppedID, succID];
+
+                var form$ = $(this).parent().find('form');
+                if (ajaxUrl) {
+                    var data = {}
+                    var es = $('input[type=hidden]', form$).get();
+                    for (var i = 0; i < es.length; ++i) {
+                        data[es[i].name] = es[i].value;
+                    }
+                    data[ 'order'] =  abbreviatedIDs.join(' ');
+                    data['dropped'] = droppedID;
+                    ui.item.addClass('ajax-loading');
+                    $.ajax(ajaxUrl, {
+                        type: 'POST',
+                        dataType: 'json',
+                        data: data
+                    }).error(function (jqXHT) {
+                        CardGrid.addMessage('Failed to store the new position of the card '
+                        + 'on the server: refresh this page to see their real positions.')
+                    }).always(function () {
+                        ui.item.removeClass('ajax-loading');
+                    });
+                } else {
+                    $('input[name=order]', form$).val(abbreviatedIDs.join(' '));
+                    $('input[name=dropped]', form$).val(droppedID);
+                }
+            }
+        }
+      })
+       .disableSelection()
+       .parent().find('form').hide();
+    }
+
+    function addCloseButtonToMessages() {
+        $('.messages').each(function (index, elt) {
+            $('<a>', {
+                href: 'javascript:void(0)',
+                text: 'close',
+                click: function () {
+                    $(elt).remove();
+                }
+            }).appendTo(elt);
+        });
     }
 
     return {
+        addCloseButtonToMessages: addCloseButtonToMessages,
         enableRearrange: enableRearrange,
         pollForUpdates: pollForUpdates,
         addMessage: addMessage
