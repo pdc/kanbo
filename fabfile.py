9768697817b5848f0c190ca818d0c4bd69579fee
pdc <pdc@alleged.org.uk>
2014-08-21 15:54:54 +0100

    Remove ‘distribute’ from requirements.txt

--- fabfile.py
+++ fabfile.py
@@ -11,7 +11,7 @@ env.settings_subdir = env.site_name
 env.django_apps = ['kanbo.about', 'kanbo.board', 'kanbo.hello']
 
 def update_requirements():
-    local("pip freeze | egrep -v 'Fabric|pycrypto|ssh|paramiko|ecdsa' > requirements.txt")
+    local("pip freeze | egrep -v 'Fabric|pycrypto|ssh|paramiko|ecdsa|distribute' > requirements.txt")
 
 def test():
     with settings(warn_only=True):
